ac2254ad6cf1d652e06667b199356360a3f1943b
Banwar <mayank54ba@gmail.com>
2022-07-14 10:43:08 +0530

    Simple Node Example

--- index.js
+++ index.js
@@ -1,15 +1,45 @@
 const rectangle=require('./rectangle');
+const rectangle=require('./square');
+const rectangle=require('./circle');
 
-function solveRect(l,b){
+function solveRect(l,b)
+{
     console.log("Solving Rectangle");
-    if(l<=0||b<=0){
+    if(l<=0||b<=0)
         console.log("Rectangle dimensions invalid");
-    }else{
+    else
+    {
         console.log("Area",rectangle.area(l,b));
         console.log("Perimeter",rectangle.perimeter(l,b));
     }
 }
 
+function solvesquare(s)
+{
+    console.log("Solving Square");
+    if(s<=0)
+        console.log("Square dimensions invalid");
+    else
+    {
+        console.log("Area",square.area(s));
+        console.log("Perimeter",square.perimeter(s));
+    }
+}
 
-solveRect(2,4);
-solveRect(5,4);
+function solveCircle(r)
+{
+    console.log("Solving Circle");
+    if(r<=0)
+        console.log("Circle dimensions invalid");
+    else
+    {
+        console.log("Area",circle.area(r));
+        console.log("Perimeter",circle.perimeter(r));
+    }
+}
+
+
+
+
+
+
---
+++ square.js
@@ -0,0 +1,2 @@
+exports.perimeter=(s)=>(4*s);
+exports.area=(s)=>(s*s);
