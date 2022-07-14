978a2a8f6be96601bbcc5c47e5dd6bb2a535d7e4
Banwar <mayank54ba@gmail.com>
2022-07-14 11:16:43 +0530

    Simple Node Example

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const rectangle=require('./rectangle');
 const square=require('./square');
 const circle=require('./circle');
+const triangle=require('./triangle');
 
 function solveRect(l,b)
 {
@@ -38,11 +39,26 @@ function solveCircle(r)
     }
 }
 
+function solvetriangle(x,y,z)
+{
+    console.log("\nSolving Triangle");
+    if(x<=0||y<=0||z<=0)
+        console.log("Triangle dimensions invalid");
+    else
+    {
+        var s=(x+y+z)/2;
+        console.log("Area",triangle.area(x,y,z,s));
+        console.log("Perimeter",triangle.perimeter(x,y,z));
+    }
+}
+
 solveRect(5,4);
 
 solvesquare(5);
 
 solveCircle(6);
+
+solvetriangle(3,4,5);
 
 
 
